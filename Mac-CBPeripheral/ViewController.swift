429b626f5c6486a462a9d3c01cb8e193ab6085e4
samuraipapa <yury.gitman@gmail.com>
2015-04-09 14:24:17 -0400

    Initial Commit

---
+++ Mac-CBPeripheral/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mac-CBPeripheral
+//
+//  Created by GrownYoda on 4/9/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
