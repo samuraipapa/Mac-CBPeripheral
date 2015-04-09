bd6b14cff21030b82852deb5e0f4a8377fae06fc
samuraipapa <yury.gitman@gmail.com>
2015-04-09 15:23:03 -0400

    most basic functionality

--- Mac-CBPeripheral/ViewController.swift
+++ Mac-CBPeripheral/ViewController.swift
@@ -7,21 +7,81 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
-class ViewController: NSViewController {
 
+
+class ViewController: NSViewController, CBPeripheralManagerDelegate {
+
+    // Core Bluetooth Stuff
+    var myPeripheralManager: CBPeripheralManager?
+    
+    // A newly generated UUID for our beacon
+    let uuid = NSUUID()
+    
+    // The ID of our beacon is the id of our bundle here
+    let identifer = "Hello ID Hello"
+    
+    let myHeartRateServiceUUID = CBUUID(string: "180D")
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        myPeripheralManager = CBPeripheralManager(delegate: self, queue: queue)
+        
+        if let manager = myPeripheralManager{
+            manager.delegate = self
 
+        }
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
+    //MARK  CBPeripheral
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+        // Stop Advertising
+        peripheral.stopAdvertising()
+        
+        // UI Stuff
+        var stateString =  String(peripheral.state.rawValue)
+        println("The peripheral state is: \(peripheral.state.hashValue)")
+        
+        
+        // Prep Advertising Packet for Periperhal
+        let manufacturerData = identifer.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+        
+        let theUUid = CBUUID(NSUUID: uuid)
+        
+        let dataToBeAdvertised:[String:AnyObject!] = [
+            CBAdvertisementDataLocalNameKey: "Hi, This is Name Message Here",
+            CBAdvertisementDataManufacturerDataKey: "Hello Hello Hello Hello",
+            CBAdvertisementDataServiceUUIDsKey: [theUUid],]
+        
+        // Start Advertising The Packet
+        peripheral.startAdvertising(dataToBeAdvertised)
+        
+        
+        
+    }
+    
+    func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager!, error: NSError!) {
+        //
+        if error == nil {
+            println("Succesfully Advertising Data")
+        } else{
+            println("Failed to Advertise Data.  Error = \(error)")
+        }
+        
+    }
+    
+    
+    
 }
-
